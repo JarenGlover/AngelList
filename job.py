0768e4f393345fa915ff2e680d8795ed751985bf
Jaren Glover <jaren.g@gmail.com>
2015-04-27 12:17:40 -0400

    everything appears to work - now to clean

--- job.py
+++ job.py
@@ -11,11 +11,12 @@ import sys
 import angel
 
 
-#test_location_info = {1629: u'Cincinnati', 2029: u'Shanghai'}
+# test_location_info = {1629: u'Cincinnati', 2029: u'Shanghai'}
 
 
 JOB_TYPE = 'full-time'
 QUALITY = 5
+JOB_LIMIT = 10
 
 
 # Code to process JSON and create a AL's API object
@@ -23,8 +24,7 @@ QUALITY = 5
 ###################
 
 def parse_input():
-
-     if not sys.stdin.isatty():
+    if not sys.stdin.isatty():
         global CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN
         try:
             cmd_input = json.load(sys.stdin)
@@ -35,82 +35,81 @@ def parse_input():
 
         except:
             raise ValueError('Error processing JSON input - must be valid JSON')
-     else:
-         raise ValueError('You must provide a JSON structured input')
+    else:
+        raise ValueError('You must provide a JSON structured input')
 
 
 def pprint(input):
     print json.dumps(input)
 
+
 def me(angel_h):
+    global USER_INFO, SKILL_INFO, LEVEL_INFO
 
-     global USER_INFO, SKILL_INFO, LEVEL_INFO
+    USER_INFO = {}
+    SKILL_INFO = {}
+    LEVEL_INFO = {}
+    # only want to call the API once
+    me_result = angel_h.get_self()
 
-     USER_INFO = {}
-     SKILL_INFO = {}
-     LEVEL_INFO = {}
-     # only want to call the API once
-     me_result = angel_h.get_self()
+    # grab location info
+    for loc in me_result['locations']:
+        #for loc in angel_h.get_self()['locations']:
+        USER_INFO[loc['id']] = loc['display_name']
 
-     # grab location info
-     for loc in me_result['locations']:
-     #for loc in angel_h.get_self()['locations']:
-         USER_INFO[loc['id']] = loc['display_name']
+    # grab skill info
+    for skill in me_result['skills']:
+        SKILL_INFO[skill['id']] = skill['name']
+        #SKILL_INFO[skill['id']]['level'] = '3' #skill['level']
+    # TODO: build levels as nested dic/JSON
 
-     # grab skill info
-     for skill in me_result['skills']:
-         SKILL_INFO[skill['id']] = skill['name']
-         #SKILL_INFO[skill['id']]['level'] = '3' #skill['level']
-     # TODO: build levels as nested dic/JSON
+    # grab levels
+    for level in me_result['skills']:
+        LEVEL_INFO[level['id']] = level['level']
 
-     # grab levels
-     for level in me_result['skills']:
-         LEVEL_INFO[level['id']] = level['level']
+    return USER_INFO, SKILL_INFO, LEVEL_INFO
 
-     return USER_INFO, SKILL_INFO, LEVEL_INFO
 
-def pagination(method,id):
-
+def pagination(method, id):
     results = []
 
     final_results = {}
     # TODO see note
-    set_env =method(id)
+    set_env = method(id)
     final_results = set_env
     last_page = set_env['last_page']
-    current_page = 66    #needs to be set to to two
+    current_page = 60  #needs to be set to to two
     job_results = {}
 
-    while current_page < last_page: #page['last_page']:
+    while current_page < last_page:  #page['last_page']:
         #job_results = request.get_tag_jobs(id, page = num)
-        job_results = method(id,page=current_page)
+        job_results = method(id, page=current_page)
         last_page = job_results['last_page']
 
         output = final_results['jobs'] + job_results['jobs']
         final_results['jobs'] = output
-
 
         current_page = current_page + 1
 
     return final_results
 
 
-def get_all_jobs(angel_h,locations):
+def get_all_jobs(angel_h, locations):
     jobs = []
     for location_id in locations:
         #print "%s location_id" % location_id
-        temp_jobs = job_search_by_location(angel_h,location_id,JOB_TYPE)
+        temp_jobs = job_search_by_location(angel_h, location_id, JOB_TYPE)
         jobs = jobs + temp_jobs
 
     return jobs
 
 
-def job_search_by_location(angel_h,location_id,job_type):
-    jobs_result = {}   #might need to remove ?
-    output =[]
+def job_search_by_location(angel_h, location_id, job_type):
+    jobs_result = {}  #might need to remove ?
+    output = []
     job_list = []
     api = angel_h.get_tag_jobs
-    jobs = pagination(api,location_id)
+    jobs = pagination(api, location_id)
     #print json.dumps(jobs)
 
     #sys.exit(555)
@@ -122,22 +121,22 @@ def job_search_by_location(angel_h,location_id,job_type):
             #print tag['display_name']
             for skill_key in SKILL_INFO.keys():
                 #print skill_key
-                if tag['id'] == skill_key and job['job_type']== job_type and job['id'] not in jobs_result.keys() \
+                if tag['id'] == skill_key and job['job_type'] == job_type and job['id'] not in jobs_result.keys() \
                         and job['startup']['quality'] > QUALITY:  # remove quality
-                   # print tag['display_name']
+                    # print tag['display_name']
                     temp_job = {'title': job['title'],
                                 'job': job['angellist_url'],
-                                'job_id':job['id'],
-                                'startup':job['startup']['name'],
-                                'quality':job['startup']['quality'],
-                                'salary_min':job['salary_min'],
-                                'salary_max':job['salary_max'],
-                                'equity_min':job['equity_min'],
-                                'equity_max':job['equity_max'],
-                                'startup_id':job['startup']['id'],
-                                'equity_vest':job['equity_vest'],
-                                'equity_cliff':job['equity_cliff'],
-                                'currency_code':job['currency_code']}
+                                'job_id': job['id'],
+                                'startup': job['startup']['name'],
+                                'quality': job['startup']['quality'],
+                                'salary_min': job['salary_min'],
+                                'salary_max': job['salary_max'],
+                                'equity_min': job['equity_min'],
+                                'equity_max': job['equity_max'],
+                                'startup_id': job['startup']['id'],
+                                'equity_vest': job['equity_vest'],
+                                'equity_cliff': job['equity_cliff'],
+                                'currency_code': job['currency_code']}
 
                     #print "temp job type %s" % type(temp_job)
                     #print temp_job
@@ -149,32 +148,50 @@ def job_search_by_location(angel_h,location_id,job_type):
                     #jobs_result['jobs_by_location'] = output
 
                     #jobs_result['job_id'] = temp_job
-    jobs_result['jobs_by_location'] = output   # REMOVE THIS?
-                    #print temp_job
-                    #if not tag['display_name'] in job_list: job_list.append(job)
-                    #print json.dumps(job_list)
-                    #sys.exit(333)
-            #sys.exit(333)
+    jobs_result['jobs_by_location'] = output  # REMOVE THIS?
+    #print temp_job
+    #if not tag['display_name'] in job_list: job_list.append(job)
+    #print json.dumps(job_list)
+    #sys.exit(333)
+    #sys.exit(333)
     #print json.dumps(jobs_result)
     return output  # rename output?
     #return len(jobs_result['jobs_by_location'])
 
+
+def check_max(input, max):
+    if input > max:
+        max = input
+    return max
+
+
 def stats(jobs):
     output = []
-    stats_output =[]
+    stats_output = []
     count = 0
     quality_total = 0
     startup_max = 0
-    equity_cliff =0
+    equity_cliff = 0
     equity_vest = 0
     equity_max = 0
-
+    max_quality_total = 0
+    max_startup_max = 0
+    max_equity_cliff = 0
+    max_equity_vest = 0
+    max_equity_max = 0
     for job in jobs:
 
         try:
+            #pprint(job)
+            max_quality_total = check_max(max_quality_total, int(job['quality']))
+            max_startup_max = check_max(max_startup_max, int(job['salary_max']))
+            max_equity_cliff = check_max(max_equity_cliff, float(job['equity_cliff']))
+            max_equity_max = check_max(max_equity_max, float(job['equity_max']))
+            max_equity_vest = check_max(max_equity_vest, float(job['equity_vest']))
+            #
             quality_total += int(job['quality'])
             startup_max += int(job['salary_max'])
-            equity_max += float(job['equity_min'])
+            equity_max += float(job['equity_max'])
             equity_vest += float(job['equity_vest'])
             equity_cliff += float(job['equity_cliff'])
             count += 1
@@ -187,9 +204,8 @@ def stats(jobs):
     # avgs
     quality_total /= count
     startup_max /= count
-    equity_max  /= count
+    equity_max /= count
     equity_cliff /= count
-
 
     '''  print '%f quality_total' % quality_total
     print '%f startup_max' % startup_max
@@ -197,26 +213,39 @@ def stats(jobs):
     print '%f equity_cliff' % equity_cliff
     print '%f equity_vest' % equity_vest
     print '%f count' % count'''
-    stats_output = {'quality_total':quality_total, 'startup_max':startup_max, 'equity_max':equity_max, 'equity_cliff':equity_cliff, 'total_count':count}
-    print stats_output['quality_total']
+    stats_output = {'avg_quality_total': quality_total, 'avg_startup_max': startup_max, 'avg_equity_max': equity_max, \
+                    'avg_equity_cliff': equity_cliff, 'total_count': count, 'max_quality_total': max_quality_total, \
+                    'max_startup_max': max_startup_max, 'max_equity_cliff': max_equity_cliff, 'max_equity_max': \
+        max_equity_max, 'max_equity_vest': max_equity_vest
+    }
+    #print stats_output['quality_total']
 
     #return {'output':output}, {'stats':{'quality_total':quality_total, 'startup_max':startup_max, 'equity_max':equity_max, 'equity_cliff':equity_cliff, 'total_count':count}}
-    return {'output':output}, {'stats_output':stats_output}
+    pprint(count)
+    pprint("+++count is about++++")
+    return {'output': output}, {'stats_output': stats_output}
     #sys.exit(3)
     #return {'output':output, 'stats_output':stats_output}
 
 
-def path_startup(api_object,jobs_input):
+def path_startup(api_object, jobs_input, stats_input):
+    #pprint(stats_input["stats_output"]["max_equity_vest"])
+
+    #sys.exit(3)
     #print type(jobs)
     #print type(args)
     #print jobs
     #print args
     #print path_input['stats_output']['total_count']
     #sys.exit(77)
+    output_handle = open("output.json", "wb")
+    final_output = []
+    rebuilt = {}
+    top_ten = {}
     count = 1
     path_avg = 0.0
     for job in jobs_input['output']:
-        path = api_object.get_paths(startup_ids=job['startup_id'],direction='followed')
+        path = api_object.get_paths(startup_ids=job['startup_id'], direction='followed')
         #path = api_object.get_paths(startup_ids=144696,direction='followed')
         #print type(path)
         #print path.keys()
@@ -227,31 +256,83 @@ def path_startup(api_object,jobs_input):
 
 
         if path:
+            #print_help = json.loads(job)
+
+            output_handle.write(json.dumps(job,indent=4,sort_keys=True))
+
+            #print type(path[str(job['startup_id'])][0])
+            #print type(path[str(job['startup_id'])])
+            #sys.exit(3)
             #pprint(path[str(job['startup_id'])])
             #pprint(len(path[str(job['startup_id'])]))
             #sys.exit(3)
+            #print path
+            count += 1
             for connect in path[str(job['startup_id'])]:
-                pprint(connect)
-                count += 1
-                path_avg += float(job['path'])
+                #for connect in path['144696']:
+                #pprint(connect)
+                #print float(len(connect))
+
+                path_avg += float(len(connect))
                 job['path'] = len(connect)
+                #job['algorithm'] = float(job['equity_vest']) * float(job['equity_cliff']) * float(job['path']) \
+                #            * float(job['salary_min']) * float(job['quality']) * float(job['equity_min'])
+                #job['algorithm_'] = float(job['equity_vest']) + float(job['equity_cliff']) + float(job['path']) \
+                #                    + float(job['salary_min']) + float(job['quality']) + float(job['equity_min'])
+                job['algorithm'] = (stats_input["stats_output"]["max_equity_vest"] - float(job['equity_vest'])) + (
+                    stats_input["stats_output"]["max_equity_cliff"] - float(job['equity_cliff'])) + float(job['path']) \
+                                   + float(job['salary_min']) + float(job['quality']) + float(job['equity_min'])
+
+                # job['algorithm'] = algorithm
+                # pprint(job)
+                # TODO
+                # Now that I know I can't del the element with no path from the org object - i need to build my own
+                # I don't think the else is useful now
 
                 #pprint(len(connect))
                 #pprint(job)
-                pprint("-----------------")
-        else:
-            #print job
-            #print job['startup_id']
+                #pprint("-----------------")
 
-            #print jobs_input['output'][str(job(['startup_id']))]
-           # startup_id = str(job(['startup_id']))
-            pprint(jobs_input['output'])
-            sys.exit(3)
-            print jobs_input['output'][34598]
+                #sys.exit(4)
+            '''
+            else:
+                #print job
+                #print job['startup_id']
+
+                #print jobs_input['output'][str(job(['startup_id']))]
+               # startup_id = str(job(['startup_id']))
+                #pprint(jobs_input['output'])
+                #sys.exit(3)
+                #print jobs_input['output'][34598]
+            '''
+            rebuilt[job['startup_id']] = job
+            final_output.append(job)
 
 
-        sys.exit(4)
-        '''
+            if len(top_ten) < 10:
+
+                top_ten[job['job_id']] = job
+                #pprint( print "This is ... %d and lenthis is %d" % (job['startup_id'], len(top_ten))
+                # pprint(top_ten)
+                #pprint("+++++")
+            else:
+                pivot = min(top_ten,key=top_ten.get)
+                if job['algorithm'] > top_ten[pivot] and len(top_ten) <= 3:
+                    top_ten.pop(pivot,None)
+                    top_ten[job['startup_id']] = job
+
+
+    #pprint(rebuilt)
+    pprint("-----------------")
+    pprint(top_ten)
+    pprint(count)
+    output_handle.close()
+    #return  rebuilt
+
+
+    #sys.exit(4)
+
+    '''
                 job['path'] = len(connect)
                 pprint(job)
                 path_avg += float(job['path'])
@@ -263,7 +344,7 @@ def path_startup(api_object,jobs_input):
 
         pprint(path_avg)
         '''
-        '''
+    '''
             #pprint(path)
             pprint(path[str(job['startup_id'][0])])
             pprint(len(path[str(job['startup_id'][0])]))
@@ -327,8 +408,9 @@ def path_startup(api_object,jobs_input):
         #print api_object.get_paths(startup_ids=jobs['startup_id'],direction='followed')['connector']
         #sys.exit(77)
 '''
-def algorithm(jobs,*args):
 
+
+def algorithm(jobs, *args):
     # unpack stats
     for arg in args:
         quality_total = arg['stats']['quality_total']
@@ -342,7 +424,7 @@ def algorithm(jobs,*args):
     for job in jobs['output']:
         print job.values()
         sys.exit(33)
-    #for j in jobs:
+        #for j in jobs:
         #print
         #print a['stats']['startup_max']
         #sys.exit(44)
@@ -350,22 +432,26 @@ def algorithm(jobs,*args):
         print job
     sys.exit(33)
 
+
 #input_4_algorithm = stats(job_search_by_location(al,1692,"full-time"))
 
 #print algorithm(*input_4_algorithm)
 
 
 
-def main ():
+def main():
     parse_input()
 
     ANGEL_API_OBJECT = angel.AngelList(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN)
     #sys.exit(44)
     LOCATION_INFO, SKILL_INFO, LEVEL_INFO = me(ANGEL_API_OBJECT)
-    jobs_output_final = get_all_jobs(ANGEL_API_OBJECT,LOCATION_INFO)
+    jobs_output_final = get_all_jobs(ANGEL_API_OBJECT, LOCATION_INFO)
 
-    jobs_final,stats_output = stats(jobs_output_final)
-    path_startup(ANGEL_API_OBJECT,jobs_final)
+    jobs_final, stats_output = stats(jobs_output_final)
+
+    #pprint(stats_output)
+    #sys.exit(3)
+    path_startup(ANGEL_API_OBJECT, jobs_final, stats_output)
     #pprint(stats_output)
     #print type(stats_output['output'])
     #pprint(path_startup(ANGEL_API_OBJECT,stats_output))
